094e2103266cf7eb4a597b25611da4d38f0a2b81
adonl <adnlwly@gmail.com>
2022-12-12 12:55:49 +0000

    refactor test to reduce repetition

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,21 +1,26 @@
 const BankAccount = require('./src/bankAccount');
 
 describe('BankAccount', () => {
+  let myAccount;
+
+  beforeEach(() => {
+    myAccount = new BankAccount;
+  })
+
   it('constructs with an empty balance of 0', () => {
-    const myAccount = new BankAccount;
     expect(myAccount.getBalance()).toEqual(0);
   });
 
   it('increases balance when funds are deposited', () => {
-    const myAccount = new BankAccount;
     myAccount.deposit(20);
     expect(myAccount.getBalance()).toEqual(20);
   });
 
   it('decreases balance when funds are withdrawn', () => {
-    const myAccount = new BankAccount;
     myAccount.deposit(20);
     myAccount.withdraw(10);
     expect(myAccount.getBalance()).toEqual(10);
-  })
+  });
+
+
 });
