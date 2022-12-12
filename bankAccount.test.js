c8646f51d53086653cf8a39e3666e2675b197eaf
adonl <adnlwly@gmail.com>
2022-12-12 13:35:05 +0000

    add getHistory method with date for single deposit

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -5,7 +5,7 @@ describe('BankAccount', () => {
 
   beforeEach(() => {
     myAccount = new BankAccount;
-  })
+  });
 
   it('constructs with an empty balance of 0', () => {
     expect(myAccount.getBalance()).toEqual(0);
@@ -20,6 +20,12 @@ describe('BankAccount', () => {
     myAccount.deposit(20);
     myAccount.withdraw(10);
     expect(myAccount.getBalance()).toEqual(10);
+  });
+
+  it('returns date with transaction when account history is requested', () => {
+    const mockDate = jest.spyOn(Date.prototype, 'toLocaleTimeString').mockReturnValue('12-12-2022');
+    myAccount.deposit(20);
+    expect(myAccount.getHistory()).toEqual("12/12/2022 20.00")
   });
 
 
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,6 +1,7 @@
 class BankAccount {
   constructor() {
     this.balance = 0;
+    this.history = [];
   }
 
   getBalance() {
@@ -8,11 +9,18 @@ class BankAccount {
   }
 
   deposit(amount) {
+    const date = new Date();
+    const todayGB = date.toLocaleDateString("en-GB");
     this.balance += amount;
+    this.history.push({amount: "20.00", date: todayGB });
   }
 
   withdraw(amount) {
     this.balance -= amount;
+  }
+
+  getHistory() {
+    return `${this.history[0].date} ${this.history[0].amount}`;
   }
 }
 
