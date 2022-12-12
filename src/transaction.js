13e33f6e53b381342c9d1a81b4d02920706539c1
adonl <adnlwly@gmail.com>
2022-12-12 16:11:38 +0000

    refactor date functions into transaction class

--- src/transaction.js
+++ src/transaction.js
@@ -1,6 +1,17 @@
 class Transaction {
   constructor() {
     this.amount = 0;
+    this.date = null;
+  }
+
+  getDate() {
+    return this.date;
+  }
+
+  setDate() {
+    const date = new Date(Date.now());
+    const todayGB = date.toLocaleDateString("en-GB");
+    return todayGB;
   }
 
   deposit(amount) {
@@ -8,6 +19,7 @@ class Transaction {
       throw 'Error - amount must be a valid number.'
     } else {
       this.amount += amount;
+      this.date = this.setDate();
     }
   }
 
--- transaction.test.js
+++ transaction.test.js
@@ -1,14 +1,28 @@
 const Transaction = require('./src/transaction');
 
 describe('Transaction', () => {
+  let transaction;
+
+  beforeEach(() => {
+    transaction = new Transaction;
+    jest
+    .spyOn(global.Date, 'now')
+    .mockImplementation(() =>
+      new Date('2022-12-25T11:01:58.135Z').valueOf()
+    );
+  });
+
   it('sets the amount of a deposit transaction', () => {
-    const transaction = new Transaction;
     transaction.deposit(20);
     expect(transaction.getAmount()).toEqual(20);
   });
 
+  it('sets the date of a deposit transaction', () => {
+    transaction.deposit(20);
+    expect(transaction.getDate()).toEqual('25/12/2022')
+  })
+
   it('throws error if deposit is not integer', () => {
-    const transaction = new Transaction;
     expect(() => transaction.deposit('reject me')).toThrow('Error - amount must be a valid number.');
   });
 
