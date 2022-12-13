289a66768453d3eace8f47e7079c75f4e7608dca
adonl <adnlwly@gmail.com>
2022-12-13 10:30:12 +0000

    update statement addTransaction test

--- statement.test.js
+++ statement.test.js
@@ -7,5 +7,13 @@ describe('Statement', () => {
     statement = new Statement;
   });
 
+  it('adds a transaction', () => {
+    statement.addTransaction('credit', '20/12/2022', 50, 75);
+    expect(statement.transactions[0].date).toEqual('20/12/2022');
+    expect(statement.transactions[0].credit).toEqual('50.00');
+    expect(statement.transactions[0].debit).toEqual("||");
+    expect(statement.transactions[0].balance).toEqual('75.00');
+  });
+
 
 });
