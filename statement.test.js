e0229298c9a3524de7ad164dfb7a610dcb1836ea
adonl <adnlwly@gmail.com>
2022-12-13 10:40:19 +0000

    add test for multiple transactions

--- statement.test.js
+++ statement.test.js
@@ -15,6 +15,17 @@ describe('Statement', () => {
     expect(statement.transactions[0].balance).toEqual('75.00');
   });
 
+  it('adds multiple transactions', () => {
+    statement.addTransaction('credit', '20/12/2022', 50, 75);
+    statement.addTransaction('credit', '22/12/2022', 25, 100);
+    statement.addTransaction('debit', '23/12/2022', 50, 50);
+    expect(statement.transactions[0].date).toEqual('20/12/2022');
+    expect(statement.transactions[1].credit).toEqual('25.00');
+    expect(statement.transactions[2].debit).toEqual("50.00");
+    expect(statement.transactions[1].balance).toEqual('100.00');
+    expect(statement.transactions[2].credit).toEqual("||");
+  });
+
   it('returns message if no transactions to format', () => {
     expect(statement.formatTransactions()).toEqual('No transactions in account history.')
   });
@@ -25,7 +36,6 @@ describe('Statement', () => {
     statement.addTransaction('debit', '23/12/2022', 50, 50);
     const result = 'date || credit || debit || balance\n20/12/2022 || 50.00 || || || 75.00\n22/12/2022 || 25.00 || || || 100.00\n23/12/2022 || || || 50.00 || 50.00';
     expect(statement.formatTransactions()).toEqual(result);
-
   });
 
 
